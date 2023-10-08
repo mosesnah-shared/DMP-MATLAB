df7a20f22f62fbdb849834f059505958434f5417
Moses C. Nah <mosesnah@mit.edu>
2023-10-08 14:04:41 -0400

    Create main_contraction_theory.m

---
+++ examples/main_contraction_theory.m
@@ -0,0 +1,7 @@
+%% [Example Script] Contraction Theory and DMP
+% [Author] Moses Chong-ook Nah
+% [Date]   2023.10.08
+
+%% [Initialization] 
+clear; close all; clc;
+fig_config( 'fontSize', 20, 'markerSize', 10 )
