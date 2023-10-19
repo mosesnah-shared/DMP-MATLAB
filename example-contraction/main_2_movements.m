0b46c0b9c3cc3a14ab299822f9b42302e811046a
Moses C. Nah <mosesnah@mit.edu>
2023-10-19 01:06:36 -0400

    updating the code and weights

--- example-contraction/main_2_movements.m
+++ example-contraction/main_2_movements.m
@@ -6,246 +6,19 @@
 clear; close all; clc;
 fig_config( 'fontSize', 20, 'markerSize', 10 )
 
-%% [1A] [Training A Single 2D Movement] 
-% There is two transformation system
-% In 2D Space, we aim to imitate the Minimum jerk Trajectory
-% The initial (p0i), final posture (p0f), duration (D), starting time (t0i) of the trajectory
-p0i = 0.0;
-p0f = 1.0;
-D   = 3.0;
-t0i = 0.0;
+%% [1A] [Calling a Weights]
 
-% For Imitation Learning, one should define the number of basis function
-N  = 50;
+% Load the dataset 
+load( 'learned_parameters/cosine.mat'   );
+data1 = data;
 
-% Parameters of the 3 DMPs
-alpha_z = 10.0;
-alpha_s = 1.0;
-beta_z  = 1/4 * alpha_z;
-tau     = D;
-y0      = p0i;
-g       = p0f;
-z0      = 0;
+load( 'learned_parameters/min_jerk.mat' );
+data2 = data;
 
-% A single canonical system
-cs        = CanonicalSystem( 'discrete', tau, alpha_s );
-trans_sys = TransformationSystem( alpha_z, beta_z, tau, y0, z0 );
-fs        = NonlinearForcingTerm( cs, N );
+load( 'learned_parameters/radial.mat'   );
+data3 = data;
 
-%% [1B] [Learning Weights via Locally Weighted Regression or Least-Square]
-
-% Assume that we have P sample points for the minimum jerk trajectory.
-P = 100;
-
-% Equal Sampling along time with duration D
-t_P = linspace( 0.0, D, P );
-
-% Desired Trajectories
-  y_des_arr = zeros( 1, P );
- dy_des_arr = zeros( 1, P );
-ddy_des_arr = zeros( 1, P );
-
-for i = 1 : P
-    [ y_des, dy_des, ddy_des ] = min_jerk_traj( t_P( i ), p0i, p0f, D, 0 );
-      y_des_arr( i ) =   y_des;
-     dy_des_arr( i ) =  dy_des;
-    ddy_des_arr( i ) = ddy_des;
-end
-
-% Calculating a_arr and f_arr that can be calculated
-f_arr = trans_sys.get_desired( y_des_arr, dy_des_arr, ddy_des_arr, g );
-
-w_arr_LSS = zeros( 1, N );
-
-phi_mat = zeros( P, N );
-for i = 1 : P
-    phi_sum = fs.calc_whole_at_t( t_P( i ) );
-    for j = 1 : N
-        phi_mat( i, j ) = fs.calc_ith( t_P( i ), j ) / phi_sum * ( g - y0 ) * cs.calc( t_P( i ) );
-    end
-end
-
-% Get w_arr with Least square solution
-tmp = ( phi_mat' * phi_mat )^(-1) * phi_mat' * f_arr';
-w_arr_LSS( : ) = tmp';
-
-%% [1C] Unrolling a plotting - Method 1
-trans_sys.reset( )
-
-% The time step of the simulation and its number of iteration
-dt = 1e-3;
-Nt = 8000;
-
-% The total time and its time array
-T     = dt * Nt;
-t_arr = dt * (0:(Nt-1));
-
-% For plotting and saving the data
-y_arr  = zeros( 1, Nt );
-dy_arr = zeros( 1, Nt );
-dz_arr = zeros( 1, Nt );
-
-y_arr( 1 ) = y0;
-z_arr( 1 ) = z0;
-
-t = 0;
-f_input_arr = zeros( 1, Nt );
-
-% One can also check spatial temporance from the weight
-% for tau = 0.5:0.1:2.0
-% for   g = 0.5:0.1:2.0
-
-for i = 0 : (Nt-1)
-    
-    % Before conducting the movement
-    % Maintaining that posture
-    if t <= t0i
-        y_arr( i + 1 ) = y0;
-        z_arr( i + 1 ) = z0;
-        
-    % During the movement
-    elseif t0i <= t
-        
-        if t<= t0i + D
-
-            % taking off the initial time offset
-            t_tmp = t - t0i;
-
-            % Calculating the input from the weights
-            % First, check if whole activation value is 0
-            phi_sum = fs.calc_whole_at_t( t_tmp );
-            
-            f_input = 0;
-
-            if phi_sum ~= 0
-                f_input = fs.calc_whole_weighted_at_t( t_tmp, w_arr_LSS )/phi_sum;
-                f_input = f_input*(g-y0)*cs.calc( t_tmp );
-            end
-            
-        else
-            f_input = 0; 
-        end
-
-        f_input_arr( i ) = f_input; 
-        [ y, z, dy, dz ] = trans_sys.step( g, f_input, dt );
-        y_arr(  i + 1 ) = y;
-        dy_arr( i + 1 ) = dy; 
-        dz_arr( i + 1 ) = dz; 
-    end
-
-    t = t + dt;
-end
-
-subplot( 3, 1, 1 )
-hold on
-plot( t_arr, y_arr, 'linewidth', 3 )
-plot( t_P, y_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
-set( gca, 'xlim', [0, T], 'fontsize', 30, 'xticklabel', {} )
-ylabel( 'Pos. (-)' )
-
-subplot( 3, 1, 2 )
-hold on
-plot( t_arr, dy_arr, 'linewidth', 3 )
-plot(   t_P, dy_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
-set( gca, 'xlim', [0, T], 'fontsize', 30, 'xticklabel', {} )
-ylabel( 'Vel. (-)' )
-
-subplot( 3, 1, 3 )
-hold on
-plot( t_arr, dz_arr/D, 'linewidth', 3 )
-plot(   t_P, ddy_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
-set( gca, 'xlim', [0, T], 'fontsize', 30, 'xtick', 0:0.5:T)
-xlabel( 'Time (sec)' )
-ylabel( 'Acc. (-)' )
-
-
-%% [1D] Unrolling a plotting - Method 2
-% From the learned weights, one can simply unroll a trajectory from
-% state-space representation. 
-
-% The time step of the simulation and its number of iteration
-dt = 1e-3;
-Nt = 8000;
-
-% The total time and its time array
-T     = dt * Nt;
-t_arr = dt * (0:(Nt-1));
-
-% First DMP
-s_arr  = zeros( 1, Nt ); 
-y_arr  = zeros( 2, Nt );
-dy_arr = zeros( 2, Nt );
-
-% Initial Position and Velocity 
-y0  = [ 4.0, 2.0 ];
-g   = [ 2.0, 3.0 ];
-dy0 = [ 0.0, 0.0 ];
-
-x_init = [ cs.calc( 0 ) ; y0'; dy0' ];
-x_arr = zeros( 5, Nt );
-
-Az = alpha_z*beta_z/tau^2*eye( 2 );
-Bz =        alpha_z/tau*eye( 2 );
-
-A1 = [   -alpha_s/tau, 0, 0, 0, 0;
-        zeros( 2, 1 ), zeros(2, 2 ), eye( 2 );
-        zeros( 2, 1 ),          -Az,    -Bz ];
-
-x_curr = x_init;
-
-t = 0;
-t0i = 1.0;
-
-% Forward Integration
-for i = 0 : (Nt-1)
-
-    % First DMP
-    if t <= t0i
-        x_arr( :, i+1 ) = x_init;
-
-    % During the movement
-    elseif t0i <= t
-
-        % taking off the initial time offset
-        t_tmp = t - t0i;
-
-        % Calculating the input from the weights
-        % First, check if whole activation value is 0
-        phi_sum1 = fs.calc_whole_at_t( t_tmp );
-        
-        f_input_x = 0;
-        if phi_sum1 ~= 0
-            f_input_x = fs.calc_whole_weighted_at_t( t_tmp, w_arr_LSS )/phi_sum1;
-            f_input_x = f_input_x*( g( 1 )-y0( 1 ) )*x_curr( 1 );
-        end
-
-        phi_sum2 = fs.calc_whole_at_t( t_tmp );
-        
-        f_input_y = 0;
-        if phi_sum2 ~= 0
-            f_input_y = fs.calc_whole_weighted_at_t( t_tmp, w_arr_LSS )/phi_sum2;
-            f_input_y = f_input_y*( g( 2 )-y0( 2 ) )*x_curr( 1 );
-        end
-        
-        if t >= t0i + D
-            f_input_x = 0;
-            f_input_y = 0;
-        end
-        
-        dx = A1 * x_curr + 1/tau^2*[ zeros(3,1); f_input_x;f_input_y ]+ [ zeros(3,1); Az*g' ];
-        x_curr = x_curr + dx * dt;
-        x_arr(:, i+1 ) = x_curr;
-        
-    end
-    t = t + dt;
-end
-
-plot( t_arr, x_arr( 2:3, : ) )
-
-
-%% [2A] Sequencing 2 Movements
-% From the learned weights, one can simply unroll a trajectory from
-% state-space representation. 
+%% [1B] Sequencing 2 Minimum-jerk Trajectory
 
 close all;
 
@@ -257,7 +30,7 @@ Nt = 6000;
 T     = dt * Nt;
 t_arr = dt * (0:(Nt-1));
 
-% First DMP
+% First Min-jerk Trajectory with DMP
 s1_arr  = zeros( 1, Nt ); 
 y1_arr  = zeros( 2, Nt );
 dy1_arr = zeros( 2, Nt );
--- examples/main_IL_save_weights.m
+++ examples/main_IL_save_weights.m
@@ -12,32 +12,87 @@ fig_config( 'fontSize', 20, 'markerSize', 10 )
 %% [1-] Imitation Learning of a Given Trajectory
 %% ---- [1A] Define Trajectory
 
+is_check = true; 
+
 % Define the Analytical Trajectory 2D that we aim to learn
-syms t
+syms t_sym
 
 % Name of the Trajectory which we aim to learn
-name_traj = 'min_jerk_traj';
+name_traj = 'radial';
 
 % Define the Trajectory, which is a 3D example.
-p0i = [ 0, 1, 1 ];
-p0f = [ 0, 2, 1 ];
-D   = 2.0;
-tn  = t/D;
+p0i = [ 0, 0, 0 ];
+D   = 3.0;
 
 % Position
-px = p0i( 1 ) + ( p0f( 1 ) - p0i( 1 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
-py = p0i( 2 ) + ( p0f( 2 ) - p0i( 2 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
-pz = p0i( 3 ) + ( p0f( 3 ) - p0i( 3 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
+switch name_traj
+
+    case 'min_jerk'
+        p0f = [ 1, 1, 1 ];      
+        tn  = t_sym/D;
+
+        px = p0i( 1 ) + ( p0f( 1 ) - p0i( 1 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
+        py = p0i( 2 ) + ( p0f( 2 ) - p0i( 2 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
+        pz = p0i( 3 ) + ( p0f( 3 ) - p0i( 3 ) ) * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
+
+    case 'cosine'
+        v  = 0.5;
+        tn = t_sym/D;        
+        px = p0i( 1 );
+        py = p0i( 2 ) + v*D * ( 10 * tn^3 - 15 * tn^4 + 6 * tn^5 );
+        pz = p0i( 3 ) + 2*( cos( 2*pi/v/D * t_sym ) - 1 );
+    
+        % Add some rotation too
+        p_tmp = [px; py; pz];
+        p_tmp = rotx( 20 ) * p_tmp;
+        p_tmp = roty( 30 ) * p_tmp;
+        p_tmp = rotz( 20 ) * p_tmp;
+
+        px = p_tmp( 1 );
+        py = p_tmp( 2 );
+        pz = p_tmp( 3 );
+
+        p0f = double( subs( p_tmp, t_sym, D )' );
+
+    case 'radial'
+        r0i = 3.0;
+        r0f = 1.0;
+
+        tn = t_sym/D;         
+
+        rt    = r0i + ( r0f - r0i ) * ( -2*tn^3 + 3*tn^2 );
+        theta = 2 * pi * ( -tn^4 + 2*tn^2 );
+
+        px = p0i( 1 );
+        py = rt * cos( theta );
+        pz = rt * sin( theta );
+
+        % Add some rotation too
+        p_tmp = [px; py; pz];
+        p_tmp = rotx( 20 ) * p_tmp;
+        p_tmp = roty( 30 ) * p_tmp;
+        p_tmp = rotz( 20 ) * p_tmp;
+
+        px = p_tmp( 1 );
+        py = p_tmp( 2 );
+        pz = p_tmp( 3 );
+
+        p0f = double( subs( p_tmp, t_sym, D )' );
+
+    otherwise
+
+end
+    
 
 % Velocity
-dpx = diff( px, t );
-dpy = diff( py, t );
-dpz = diff( pz, t );
+dpx = diff( px, t_sym );
+dpy = diff( py, t_sym );
+dpz = diff( pz, t_sym );
 
 % Acceleration
-ddpx = diff( dpx, t );
-ddpy = diff( dpy, t );
-ddpz = diff( dpz, t );
+ddpx = diff( dpx, t_sym );
+ddpy = diff( dpy, t_sym );
+ddpz = diff( dpz, t_sym );
 
 % Saving these elements as symbolic array.
 p_sym   = {   px,   py,   pz };
@@ -56,11 +111,27 @@ ddp_data = zeros( 3, P );
 
 for i = 1 : P
     for j = 1 : 3
-         p_data( j, i ) = subs(   p_sym{ j }, t, t_arr( i ) );
-        dp_data( j, i ) = subs(  dp_sym{ j }, t, t_arr( i ) );
-       ddp_data( j, i ) = subs( ddp_sym{ j }, t, t_arr( i ) );
+         p_data( j, i ) = double( subs(   p_sym{ j }, t_sym, t_arr( i ) ) );
+        dp_data( j, i ) = double( subs(  dp_sym{ j }, t_sym, t_arr( i ) ) );
+       ddp_data( j, i ) = double( subs( ddp_sym{ j }, t_sym, t_arr( i ) ) );
     end
 end
+
+if is_check 
+    f = figure( );
+    p1 = subplot( 3, 1, 1 );
+    plot( p1, t_arr,   p_data );
+
+    p2 = subplot( 3, 1, 2 );
+    plot( p2, t_arr,  dp_data );
+    
+    p3 = subplot( 3, 1, 3 );
+    plot( p3, t_arr,  ddp_data );
+    
+end
+
+f = figure( ); a = axes( 'parent', f );
+plot3( a, p_data( 1, : ), p_data( 2, : ), p_data( 3, : ))
 
 %% ---- [1B] Learning the Weights for Imitation Learning
 
@@ -68,185 +139,199 @@ end
 N  = 50;
 
 % Parameters of the 3 DMPs
+% We use the identical alpha_z, beta_z values
 alpha_z = 10.0;
 alpha_s = 1.0;
 beta_z  = 1/4 * alpha_z;
 tau     = D;
-g       = q0f;
-y0      = q0i;
 z0      = 0;
 
-cs        = CanonicalSystem( 'discrete', tau, alpha_s );
-trans_sys = TransformationSystem( alpha_z, beta_z, tau, y0, z0 );
-fs        = NonlinearForcingTerm( cs, N );
+cs  = CanonicalSystem( 'discrete', tau, alpha_s );
 
-%% ---- [1B] Learning Weights via Locally Weighted Regression or Least-Square
+tsx = TransformationSystem( alpha_z, beta_z, tau, p_data( 1, 1 ), dp_data( 1, 1 )*tau );
+tsy = TransformationSystem( alpha_z, beta_z, tau, p_data( 2, 1 ), dp_data( 2, 1 )*tau );
+tsz = TransformationSystem( alpha_z, beta_z, tau, p_data( 3, 1 ), dp_data( 3, 1 )*tau );
+ts_arr = { tsx, tsy, tsz };
 
-% Assume that we have P sample points for the minimum jerk trajectory.
-P = 100;
+fs = NonlinearForcingTerm( cs, N );
 
-% Equal Sampling along time with duration D
-t_P = linspace( 0.0, D, P );
 
-% Desired Trajectories
-  y_des_arr = zeros( 1, P );
- dy_des_arr = zeros( 1, P );
-ddy_des_arr = zeros( 1, P );
+%% ---- [1C] Learning Weights via Least-Square
 
-for j = 1 : P
-    [ y_des, dy_des, ddy_des ] = min_jerk_traj( t_P( j ), q0i, q0f, D, 0 );
-      y_des_arr( j ) =   y_des;
-     dy_des_arr( j ) =  dy_des;
-    ddy_des_arr( j ) = ddy_des;
-end
+w_arr = zeros( 3, N );
 
-% Learning the N weights based on Locally Weighted Regression
-w_arr_LWR = zeros( 1, N );
+for i = 1 : 3
 
-% Calculating a_arr and f_arr that can be calculated
-a_arr = ( g - y0 ) * cs.calc( t_P );
-f_arr = trans_sys.get_desired( y_des_arr, dy_des_arr, ddy_des_arr, g );
-
-% Iterating over the arrays
-for i = 1: N
-    phi_arr = fs.calc_ith( t_P, i );    
-    w_arr_LWR( i ) = sum( a_arr .* phi_arr .* f_arr ) / sum( a_arr .* phi_arr .* a_arr );
-end
-
-a1 = subplot( 2, 1, 1 );
-hold( a1, 'on' );
-
-% Plotting the Desired Trajectory
-plot( a1, t_P,   y_des_arr );
-plot( a1, t_P,  dy_des_arr );
-plot( a1, t_P, ddy_des_arr );
- 
-a2 = subplot( 2, 1, 2 );
-hold( a2, 'on' );
-for i = 1 : N
-    plot( a2, t_P, fs.calc_whole_weighted_at_t( t_P, w_arr_LWR ) )
-end
-
-% One can also learn the weights by simple least-square solution
-% It is simply f_arr = A w problem, where
-% f_arr is simply the array above. 
-phi_mat = zeros( P, N );
-
-for i = 1 : P
-    phi_sum = fs.calc_whole_at_t( t_P( i ) );
-    for j = 1 : N
-        phi_mat( i, j ) = fs.calc_ith( t_P( i ), j ) / phi_sum * ( g - y0 ) * cs.calc( t_P( i ) );
+    if p0f( i ) - p0i( i ) == 0
+        continue
     end
+
+    phi_mat = zeros( P, N );    
+    f_arr = ts_arr{ i }.get_desired( p_data( i, : ), dp_data( i, : ), ddp_data( i, : ), p0f( i ) );
+
+    % Over the Time Array
+    for j = 1 : P
+        phi_sum = fs.calc_whole_at_t( t_arr( j ) );
+
+        % Over the time basis function
+        for k = 1 : N
+            phi_mat( j, k ) = fs.calc_ith( t_arr( j ), k ) / phi_sum * ( p0f( i ) - p0i( i ) ) * cs.calc( t_arr( j ) );
+        end
+    end
+
+    % Get w_arr with Least square solution
+    ttmp = ( phi_mat' * phi_mat )^(-1) * phi_mat' * f_arr';
+    w_arr( i, : ) = ttmp';
+
 end
 
-% Get w_arr with Least square solution
-w_arr_LSS = ( phi_mat' * phi_mat )^(-1) * phi_mat' * f_arr';
-w_arr_LSS = w_arr_LSS';
+%% ---- [1D] Rollout Generating a Full trajectory with the Transformation System
 
-%% ---- [1C] Rollout Generating a Full trajectory with the Transformation System
+% Resetting the Transformation System
+for i = 1 : 3
+    ts_arr{ i }.reset( )
+end
 
-trans_sys.reset( )
-
-% The time step of the simulation and its number of iteration
-dt = 1e-3;
-Nt = 2000;
+% Initial Time and time step
+t0i = 2.0;
+dt  = 1e-3;
+Nt  = 8000;
 
 % The total time and its time array
-T     = dt * Nt;
-t_arr = dt * (0:(Nt-1));
+T      = dt * Nt;
+t_arr2 = dt * (0:(Nt-1));
 
 % For plotting and saving the data
-y_arr  = zeros( 1, Nt );
-z_arr  = zeros( 1, Nt );
-dz_arr = zeros( 1, Nt );
+y_arr  = zeros( 3, Nt );
+z_arr  = zeros( 3, Nt );
+dy_arr = zeros( 3, Nt );
+dz_arr = zeros( 3, Nt );
 
-y_arr( 1 ) = y0;
-z_arr( 1 ) = z0;
+% Initial Condition
+ y_arr( :, 1 )  =  p_data( :, 1 );
+dy_arr( :, 1 )  = dp_data( :, 1 );
+ z_arr( :, 1 )  = dp_data( :, 1 ) * tau;
+
+% The analytical data 
+  p_arr_test = zeros( 3, Nt );
+ dp_arr_test = zeros( 3, Nt );
+ddp_arr_test = zeros( 3, Nt );
 
 t = 0;
-f_input_arr = zeros( 1, Nt );
 
-% One can also check spatial temporance from the weight
-% for tau = 0.5:0.1:2.0
-% for   g = 0.5:0.1:2.0
 
 for i = 0 : (Nt-1)
     
-    % Before conducting the movement
-    % Maintaining that posture
-    if t <= t0i
-        y_arr( i + 1 ) = y0;
-        z_arr( i + 1 ) = z0;
-        
-    % During the movement
-    elseif t0i <= t
-        
-        if t<= t0i + D
+    for k = 1 : 3
 
-            % taking off the initial time offset
-            t_tmp = t - t0i;
-
-            % Calculating the input from the weights
-            % First, check if whole activation value is 0
-            phi_sum = fs.calc_whole_at_t( t_tmp );
-            
-            f_input = 0;
-
-            if phi_sum ~= 0
-                f_input = fs.calc_whole_weighted_at_t( t_tmp, w_arr_LWR )/phi_sum;
-                f_input = f_input*(g-y0)*cs.calc( t_tmp );
-            end
-            
-        else
-            f_input = 0; 
+        if sum( abs( w_arr( k, : ) ) ) == 0
+            continue;
         end
 
-        f_input_arr( i ) = f_input; 
-        [ y, z, dy, dz ] = trans_sys.step( g, f_input, dt );
-        y_arr(  i + 1 ) = y;
-        z_arr(  i + 1 ) = z;
-        dz_arr( i + 1 ) = dz;        
+        % Before conducting the movement
+        % Maintaining that posture
+        if t <= t0i
+            y_arr( k, i + 1 ) =  y_arr( k, 1 );
+           dy_arr( k, i + 1 ) = dy_arr( k, 1 );            
+            z_arr( k, i + 1 ) =  z_arr( k, 1 );
+    
+              p_arr_test( k, i + 1 ) =   p_data( k, 1 );
+             dp_arr_test( k, i + 1 ) =  dp_data( k, 1 );
+            ddp_arr_test( k, i + 1 ) = ddp_data( k, 1 );
+    
+        % During the movement
+        elseif t0i <= t
+            
+            if t<= t0i + D
+    
+                % taking off the initial time offset
+                t_tmp = t - t0i;
+    
+                % Calculating the input from the weights
+                % First, check if whole activation value is 0
+                phi_sum = fs.calc_whole_at_t( t_tmp );
+                
+                f_input = 0;
+    
+                if phi_sum ~= 0
+                    f_input = fs.calc_whole_weighted_at_t( t_tmp, w_arr( k, : ) )/phi_sum;
+                    f_input = f_input*( p0f( k )- p0i( k ) )*cs.calc( t_tmp );
+                end
+                
+                % Position Array
+                  p_arr_test( k, i + 1 ) = double( subs(   p_sym{ k }, t_sym, t_tmp ) );
+                 dp_arr_test( k, i + 1 ) = double( subs(  dp_sym{ k }, t_sym, t_tmp ) );
+                ddp_arr_test( k, i + 1 ) = double( subs( ddp_sym{ k }, t_sym, t_tmp ) );       
+    
+            else
+                f_input = 0; 
+    
+                  p_arr_test( k, i + 1 ) =   p_data( k, end );
+                 dp_arr_test( k, i + 1 ) =    0;
+                ddp_arr_test( k, i + 1 ) =    0;
+            end
+    
+            [ y, z, dy, dz ] = ts_arr{ k }.step( p0f( k ), f_input, dt );
+            y_arr(  k, i + 1 ) = y;
+            z_arr(  k, i + 1 ) = z; 
+            dy_arr( k, i + 1 ) = dy;
+            dz_arr( k, i + 1 ) = dz;
+    
+        end
+
     end
 
     t = t + dt;
 end
 
+%% [1E] Plotting the Results
+% Plot the results
+f = figure( );
 subplot( 3, 1, 1 )
 hold on
-plot( t_arr, y_arr, 'linewidth', 3 )
-plot( t_P, y_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
+plot( t_arr2, y_arr, 'linewidth', 3 )
+plot( t_arr2, p_arr_test, 'linewidth', 3, 'linestyle', '--' )
 set( gca, 'xlim', [0, T], 'fontsize', 30, 'xticklabel', {} )
 ylabel( 'Pos. (-)' )
 
 subplot( 3, 1, 2 )
 hold on
-plot( t_arr, z_arr, 'linewidth', 3 )
-plot(   t_P, dy_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
+plot( t_arr2, dy_arr, 'linewidth', 3 )
+plot( t_arr2, dp_arr_test, 'linewidth', 3, 'linestyle','--' )
 set( gca, 'xlim', [0, T], 'fontsize', 30, 'xticklabel', {} )
 ylabel( 'Vel. (-)' )
 
 subplot( 3, 1, 3 )
 hold on
-plot( t_arr, dz_arr, 'linewidth', 3 )
-plot(   t_P, ddy_des_arr, 'linestyle', '--', 'linewidth', 5, 'color', 'k' )
+plot( t_arr2, dz_arr/tau, 'linewidth', 3 )
+plot( t_arr2, ddp_arr_test, 'linewidth', 3, 'linestyle','--' )
 set( gca, 'xlim', [0, T], 'fontsize', 30, 'xtick', 0:0.5:T)
 xlabel( 'Time (sec)' )
 ylabel( 'Acc. (-)' )
 
-%% ---- [1D] Saving the Weights and Parameters of the Learned Trajectory 
+figure( )
+plot3( p_arr_test( 1, : ), p_arr_test( 2, : ), p_arr_test( 3, : ), 'color', 'k' )
 
-% All the necessary data 
+%% [1F] Saving the Data
+
+% The Weighting Matrix and additional Data
 data = struct;
+data.p_sym   =   p_sym;
+data.dp_sym  =  dp_sym;
+data.ddp_sym = ddp_sym;
 
+data.name = name_traj;
 data.alpha_z = alpha_z;
-data.beta_z  = beta_z;
+data.beta_z  =  beta_z;
+data.alpha_s = alpha_s;
 
-data.cs        = cs;
-data.trans_sys = trans_sys;
-data.fs        = fs;
-data.g         = g;
+data.weight  = w_arr;
+data.p0i = p0i;
+data.p0f = p0f;
+data.tau = tau;
 
-data.w_arr_LSS = w_arr_LSS;
-data.w_arr_LWR = w_arr_LWR;
+data.cs = cs;
+data.fs = fs;
+data.ts_arr = ts_arr;
 
-% save( './learned_parameters/min_jerk_traj' , 'data' );
+save( ['learned_parameters/', name_traj ,'.mat'], 'data' );
